691675c683f649eba9e303dab27f7eca88161f06
stifskere <stifskere@gmail.com>
2025-01-04 00:07:54 +0100

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+	import { ref } from "vue";
+
+	const count = ref(0);
+</script>
+
+<script onmounted lang="ts">
+	export default {
+		name: "App"
+	};
+</script>
+
+<template>
+	<button @click="count++">{{ count }}</button>
+</template>
